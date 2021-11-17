63b812ee593e53b02eecbe151ac34ded8b7caff1
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-17 11:35:57 +0200

    Refactor App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavigationBar from "./components/NavigationBar";
+import Layout from "./components/Layout";
 import Home from "./components/Home";
 import About from "./components/About";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <NavigationBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
