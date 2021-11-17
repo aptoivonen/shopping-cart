bea9d27f562a733e960d02ae4dd290a5fce2b59c
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-17 10:16:41 +0200

    Set up basic file structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar";
+import Home from "./components/Home";
+import About from "./components/About";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
