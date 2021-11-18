1d0019f7dd54b170a2cc7955f76841908ee68fd8
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-18 12:35:20 +0200

    Start app logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useReducer } from "react";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Products from "./components/Products";
 import DATA from "./data/data";
 
+const ACTIONS = {
+  ADD_PRODUCT: "addProduct",
+  REMOVE_PRODUCT: "removeProduct",
+  // TODO: increment/decrement quantity or setQuantity action
+};
+
+const reducer = (products, { type, payload }) => {
+  switch (type) {
+    case ACTIONS.ADD_PRODUCT:
+      const foundProduct = DATA.find((product) => product.id === payload.id);
+      let newProducts;
+      if (!foundProduct) {
+        newProducts = [...products, { id: payload.id, quantity: 1 }];
+      } else {
+        newProducts = products.map((product) =>
+          product.id === payload.id
+            ? { id: product.id, quantity: product.quantity + 1 }
+            : product
+        );
+      }
+      return newProducts;
+    case ACTIONS.REMOVE_PRODUCT:
+      return products.filter((product) => product.id === payload.id);
+    default:
+      throw new Error(`No such action type: ${type}`);
+  }
+};
+
 function App() {
+  const [products, dispatch] = useReducer(reducer, []);
+
   return (
     <BrowserRouter>
       <Routes>
