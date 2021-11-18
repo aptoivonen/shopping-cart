d8abdce902b5f296404ce9da805daa3e64ae6814
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-18 12:35:00 +0200

    Refactor Products to use data

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,7 +1,12 @@
-const Products = () => {
+const Products = ({ products }) => {
   return (
     <>
       <h1>Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
     </>
   );
 };
