48c585fa40ca175080a180478cb79e90c5fa7eac
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-18 16:25:10 +0200

    Refactor hooks to own folder

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { NavLink } from "react-router-dom";
-import useCart from "./useCart";
+import useCart from "../hooks/useCart";
 import Cart from "./Cart";
 
 const NavigationBar = () => {
