a356310be8c02deb0d00fa99db64b10fa2d4251e
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-18 16:30:03 +0200

    Add price to data

--- src/data/data.js
+++ src/data/data.js
@@ -4,24 +4,28 @@ const data = [
     title: "Original T-Shirt",
     description: "This t-shirt has attitude!",
     img: "https://unsplash.com/photos/fEt6Wd4t4j0",
+    price: 1999,
   },
   {
     id: 2,
     title: "Leave the Road T-Shirt",
     description: "This t-shirt has attitude!",
     img: "https://unsplash.com/photos/VW5VjskNXZ8",
+    price: 2499,
   },
   {
     id: 3,
     title: "Plain White T-Shirt",
     description: "This t-shirt has attitude!",
     img: "https://unsplash.com/photos/m1MRYp556Ew",
+    price: 999,
   },
   {
     id: 4,
     title: "Fucking Awesome T-Shirt",
     description: "This t-shirt has attitude!",
     img: "https://unsplash.com/photos/2XcbGfYShfk",
+    price: 2999,
   },
 ];
 
