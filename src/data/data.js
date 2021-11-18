b9fee7b8e91e35aec4c34be8561d8d010e448a35
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-18 11:50:40 +0200

    Make some data

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: 1,
+    title: "Original T-Shirt",
+    description: "This t-shirt has attitude!",
+    img: "https://unsplash.com/photos/fEt6Wd4t4j0",
+  },
+  {
+    id: 2,
+    title: "Leave the Road T-Shirt",
+    description: "This t-shirt has attitude!",
+    img: "https://unsplash.com/photos/VW5VjskNXZ8",
+  },
+  {
+    id: 3,
+    title: "Plain White T-Shirt",
+    description: "This t-shirt has attitude!",
+    img: "https://unsplash.com/photos/m1MRYp556Ew",
+  },
+  {
+    id: 4,
+    title: "Fucking Awesome T-Shirt",
+    description: "This t-shirt has attitude!",
+    img: "https://unsplash.com/photos/2XcbGfYShfk",
+  },
+];
+
+export default data;
