cf0a53a97f2c0702c9471ec7dc5a5d8d30ef99a9
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-18 17:39:09 +0200

    Add product functionality to Products

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,28 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { ACTIONS } from "../hooks/useCart";
+
 const Products = ({ products }) => {
+  const { dispatch } = useContext(CartContext);
+
   return (
     <>
       <h1>Products</h1>
       <ul>
         {products.map((product) => (
-          <li key={product.id}>{product.title}</li>
+          <li key={product.id}>
+            <p>{product.title}</p>
+            <button
+              onClick={() => {
+                dispatch({
+                  type: ACTIONS.ADD_PRODUCT,
+                  payload: { id: product.id },
+                });
+              }}
+            >
+              Add to cart
+            </button>
+          </li>
         ))}
       </ul>
     </>
