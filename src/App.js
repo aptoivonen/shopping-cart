bb952f6c5a5c920db6e2f9c1c0115ccee6fc3106
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-22 10:35:11 +0200

    Move BrowserRouter to index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./components/Home";
 import Products from "./components/Products";
-import { CartProvider } from "./context/CartContext";
 import NoMatch from "./components/NoMatch";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <CartProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="shop" element={<Products />} />
-            <Route path="*" element={<NoMatch />} />
-          </Route>
-        </Routes>
-      </CartProvider>
-    </BrowserRouter>
+    <CartProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="shop" element={<Products />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </CartProvider>
   );
 }
 
