b52ca3ac9b912175fd2abebf82f661777fa4ca61
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-23 16:56:04 +0200

    Add tooltip to cart icon

--- src/components/CartIcon.js
+++ src/components/CartIcon.js
@@ -3,6 +3,9 @@ import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { countNumberOfProducts } from "../hooks/useCart";
 import "./CartIcon.scss";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
+
+const pluralPrefix = (numberOfThings) => (numberOfThings > 1 ? "s" : "");
 
 const CartIcon = () => {
   const {
@@ -10,15 +13,27 @@ const CartIcon = () => {
   } = useContext(CartContext);
   const numberOfCartProducts = countNumberOfProducts(cartProducts);
 
+  const renderCartIconTooltip = (props) => (
+    <Tooltip id="cart-tooltip" {...props}>
+      Click to view Your shopping cart.{" "}
+      {numberOfCartProducts > 0 &&
+        `You have ${numberOfCartProducts} product${pluralPrefix(
+          numberOfCartProducts
+        )} in Your cart.`}
+    </Tooltip>
+  );
+
   return (
-    <div className="d-flex">
-      <FaShoppingCart />
-      {numberOfCartProducts > 0 && (
-        <div className="cartIcon d-flex justify-content-center align-items-center ms-1 rounded-circle bg-light text-primary fw-bold">
-          {numberOfCartProducts}
-        </div>
-      )}
-    </div>
+    <OverlayTrigger placement="bottom" overlay={renderCartIconTooltip}>
+      <div className="d-flex">
+        <FaShoppingCart />
+        {numberOfCartProducts > 0 && (
+          <div className="cartIcon d-flex justify-content-center align-items-center ms-1 rounded-circle bg-light text-primary fw-bold">
+            {numberOfCartProducts}
+          </div>
+        )}
+      </div>
+    </OverlayTrigger>
   );
 };
 
