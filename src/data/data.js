97962eae794811a94a93db7ae96ca61f4e4f1627
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-25 12:33:28 +0200

    Fix data

--- src/data/data.js
+++ src/data/data.js
@@ -1,30 +1,32 @@
+const IMAGE_WIDTH = 200;
+
 const data = [
   {
     id: 1,
     title: "Original T-Shirt",
     description: "This t-shirt has attitude!",
-    img: "https://unsplash.com/photos/fEt6Wd4t4j0",
+    img: `https://images.unsplash.com/photo-1576566588028-4147f3842f27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=${IMAGE_WIDTH}&q=80`,
     price: 1999,
   },
   {
     id: 2,
     title: "Leave the Road T-Shirt",
     description: "This t-shirt has attitude!",
-    img: "https://unsplash.com/photos/VW5VjskNXZ8",
+    img: `https://images.unsplash.com/photo-1554568218-0f1715e72254?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=${IMAGE_WIDTH}&q=80`,
     price: 2499,
   },
   {
     id: 3,
     title: "Plain White T-Shirt",
     description: "This t-shirt has attitude!",
-    img: "https://unsplash.com/photos/m1MRYp556Ew",
+    img: `https://images.unsplash.com/photo-1586790170083-2f9ceadc732d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=${IMAGE_WIDTH}&q=80`,
     price: 999,
   },
   {
     id: 4,
     title: "Fucking Awesome T-Shirt",
     description: "This t-shirt has attitude!",
-    img: "https://unsplash.com/photos/2XcbGfYShfk",
+    img: `https://images.unsplash.com/photo-1571945153237-4929e783af4a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=${IMAGE_WIDTH}&q=80`,
     price: 2999,
   },
 ];
