bceefb1883a93b379e53f55f5af6475d096c2130
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-26 11:28:58 +0200

    Make card image responsive

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,16 +1,11 @@
 import { Card } from "react-bootstrap";
 import { formatAndPrefixCurrency } from "../utils/currency";
-
-// TODO: make image height responsive, > 500px xs-breakpoint
+import "./ShopItem.scss";
 
 const ShopItem = ({ product: { id, title, description, img, price } }) => {
   return (
     <Card>
-      <Card.Img
-        variant="top"
-        src={img}
-        style={{ height: 500, objectFit: "cover" }}
-      />
+      <Card.Img className="shop-card-image" variant="top" src={img} />
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Text>{formatAndPrefixCurrency(price)}</Card.Text>
