8498d2314351bea39b4340d2053960a221e2b21a
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-26 16:41:09 +0200

    Introduce a styled link to shop item

--- src/components/ShopItem.js
+++ src/components/ShopItem.js
@@ -1,16 +1,19 @@
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { formatAndPrefixCurrency } from "../utils/currency";
 import "./ShopItem.scss";
 
-const ShopItem = ({ product: { id, title, description, img, price } }) => {
+const ShopItem = ({ product: { id, title, img, price } }) => {
   return (
-    <Card>
-      <Card.Img className="shop-card-image" variant="top" src={img} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{formatAndPrefixCurrency(price)}</Card.Text>
-      </Card.Body>
-    </Card>
+    <Link className="text-decoration-none text-reset" to="">
+      <Card>
+        <Card.Img className="shop-card-image" variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{formatAndPrefixCurrency(price)}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 
