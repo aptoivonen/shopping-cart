cf02f77d5e4389369a74a9385422d6af5b5fb3e1
Aleksi Toivonen <aptoivonen@mail.com>
2021-11-30 19:15:18 +0200

    Create cart layout

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,14 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { ACTIONS } from "../hooks/useCart";
+import CartItem from "./CartItem";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { NavLink } from "react-router-dom";
-import CartItem from "./CartItem";
+import { countSubTotal } from "../utils/products";
+import { formatAndPrefixCurrency } from "../utils/currency";
 
 const Cart = () => {
   const {
@@ -17,26 +21,43 @@ const Cart = () => {
   };
 
   const emptyCartTemplate = (
-    <div>
-      <p>Your cart is empty. Add some products to it.</p>
+    <>
+      <p className="lead">Your cart is empty. Add some products to it.</p>
       <Button as={NavLink} to="/shop" variant="primary" onClick={closeCart}>
         Go Shopping
       </Button>
-    </div>
+    </>
   );
 
   const filledCartTemplate = (
-    <ul>
-      {products.map((product) => (
-        <CartItem key={product.id} product={product} />
-      ))}
-    </ul>
+    <>
+      <>
+        {products.map((product, index) => (
+          <CartItem
+            key={product.id}
+            product={product}
+            last={index === products.length - 1}
+          />
+        ))}
+      </>
+      <Row className="py-3 border-top">
+        <Col xs={9}>Subtotal:</Col>
+        <Col xs={3}>{formatAndPrefixCurrency(countSubTotal(products))}</Col>
+      </Row>
+      <Row>
+        <Col className="d-grid">
+          <Button variant="secondary" size="lg">
+            Continue to Checkout
+          </Button>
+        </Col>
+      </Row>
+    </>
   );
 
   return (
     <Offcanvas show={showCart} onHide={closeCart} placement="end">
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title>Your Cart</Offcanvas.Title>
+      <Offcanvas.Header className="border-bottom" closeButton>
+        <Offcanvas.Title className="fw-bold">Your Cart</Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
         {products.length > 0 ? filledCartTemplate : emptyCartTemplate}
--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,8 +1,14 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
 import { ACTIONS } from "../hooks/useCart";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { formatAndPrefixCurrency } from "../utils/currency";
+import { FaPlus, FaMinus } from "react-icons/fa";
+import "./CartItem.scss";
 
-const CartItem = ({ product: { id, title, quantity } }) => {
+const CartItem = ({ product: { id, title, price, img, quantity }, last }) => {
   const { dispatch } = useContext(CartContext);
 
   const handleDecreaseQuantity = () => {
@@ -27,14 +33,49 @@ const CartItem = ({ product: { id, title, quantity } }) => {
   };
 
   return (
-    <li>
-      <p style={{ fontWeight: "bold" }}>{title}</p>
-      <p>
-        Qty: <button onClick={handleDecreaseQuantity}>-</button> {quantity}
-        <button onClick={handleIncreaseQuantity}>+</button>
-      </p>
-      <button onClick={handleRemove}>Remove</button>
-    </li>
+    <Row className={`pb-2 mb-2 ${last ? "" : "border-bottom"}`}>
+      <Col xs={3}>
+        <img className="img-fluid cart-item-img" src={img} alt={title} />
+      </Col>
+      <Col>
+        <Row className="mb-1">
+          <Col>
+            <span className="fw-bold">{title}</span>
+          </Col>
+        </Row>
+        <Row className="mb-1">
+          <Col>
+            <Button
+              size="sm"
+              variant="outline-info"
+              onClick={handleDecreaseQuantity}
+            >
+              <FaMinus />
+            </Button>
+            <span className="cart-item-quantity d-inline-flex align-items-center justify-content-center border btn btn-sm">
+              {quantity}
+            </span>
+            <Button
+              size="sm"
+              variant="outline-info"
+              onClick={handleIncreaseQuantity}
+            >
+              <FaPlus />
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Button size="sm" variant="outline-danger" onClick={handleRemove}>
+              Remove
+            </Button>
+          </Col>
+        </Row>
+      </Col>
+      <Col xs="auto">
+        <p>{formatAndPrefixCurrency(price)}</p>
+      </Col>
+    </Row>
   );
 };
 
